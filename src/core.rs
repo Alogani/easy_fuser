fce0f4a7e240ce131af45129340f63b95f0ef777
clement@desktop <clement@desktop>
2026-02-01 14:13:56 +0000

    fuse_driver methods templify

--- src/core.rs
+++ src/core.rs
@@ -1,8 +1,7 @@
 mod fuse_driver;
-mod fuse_driver_types;
+mod helpers;
 mod inode_mapping;
-mod macros;
 mod thread_mode;
 
-pub(crate) use fuse_driver_types::FuseDriver;
+pub(crate) use fuse_driver::FuseDriver;
 pub(crate) use inode_mapping::{InodeResolvable, ROOT_INO};
