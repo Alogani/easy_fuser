4bb071a988ec888e9cdde6ae89d9d7135fabf50f
Alogani <77880767+Alogani@users.noreply.github.com>
2026-02-08 19:28:03 +0100

    Bsd support (#92)
    * Add bsd workflow
    
    * Modify unix_fs mod to support bsd operations internally

--- tests/test_recursion.rs
+++ tests/test_recursion.rs
@@ -75,9 +75,21 @@ fn test_mirror_fs_recursion() {
     println!("Test passed: No infinite recursion detected.");
 
     eprintln!("Unmounting filesystem...");
-    let _ = std::process::Command::new("fusermount")
-        .arg("-u")
-        .arg(&mntpoint)
-        .status();
-    handle.join().unwrap();
+    #[cfg(not(target_os = "freebsd"))]
+    {
+        let _ = std::process::Command::new("fusermount")
+            .arg("-u")
+            .arg(&mntpoint)
+            .status();
+        handle.join().unwrap();
+    }
+    #[cfg(target_os = "freebsd")] // TODO: explore why error: no such file or directory happens there
+    {
+        let _ = std::process::Command::new("umount")
+            .arg(&mntpoint)
+            .status();
+        let _ = handle.join();
+    }
+    drop(mntpoint);
+    
 }
